afb7d313bbf9b4ca6561ba39d31c6d03a5961196
Neetesh NG <neeteshng15@gmail.com>
2023-11-02 10:44:25 +0530

    Added button for git Repositoty

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="App">
       <TodoList/>
+      <div className="ul_git">
+            <div className="il_git">
+              <a
+                href="https://github.com/NeeteshNG/MyTaskBestPeers_3"
+                rel="noreferrer"
+                target="_blank"
+                title="Git Repo"
+                className="git-click"
+              >
+                <i className="fab fa-github size_git"></i>
+              </a>
+            </div>
+          </div>
     </div>
   );
 }
